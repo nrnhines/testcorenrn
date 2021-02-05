a8851af185e5b78724a7f2c618840bd2aacbaed6
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-02-05 12:26:28 +0100

    Fixed existing tests for spike comparissons

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+rm -rf *dat x86_64 *.spk
 
 export OMP_NUM_THREADS=1
 
@@ -16,10 +18,16 @@ for test in "${!mpi_ranks[@]}" ; do
   mkdir -p test${test}dat
 done
 
+~/bbp_repos/nrn/build/install/bin/nrnivmodl -coreneuron mod
+
 for test in "${!mpi_ranks[@]}" ; do
   echo "Running neuron for $test"
   num_ranks=${mpi_ranks[$test]}
-  srun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 test${test}.hoc
+  if [ "${test}" = "patstim" ]; then
+    mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 -c dumpmodel=1 test${test}.hoc
+  else
+    mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 test${test}.hoc
+  fi
   cat out${test}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${test}.spk
   rm out${test}.dat
 done
@@ -28,14 +36,14 @@ done
 for test in "${!mpi_ranks[@]}" ; do
   echo "Running coreneuron for $test"
   num_ranks=${mpi_ranks[$test]}
-  if [ "${test}" = "patstim" ]
-  then
-    srun -n $num_ranks ./x86_64/special-core -mpi -d test${test}dat -e 100 --pattern patstim.spk
+  if [ "${test}" = "patstim" ]; then
+    mpirun -n $num_ranks ./x86_64/special-core --mpi --mpi -d coredat -e 100 --pattern patstim.spk
+    mv out.dat out${test}.dat
   else
-    srun -n $num_ranks ./x86_64/special-core -mpi -d test${test}dat -e 100
+    mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 -c coreneuron=1 test${test}.hoc
   fi
-  cat out.dat > out_cn_${test}.spk
-  rm out.dat
+  cat out${test}.dat | sort -k 1n,1n -k 2n,2n > out_cn_${test}.spk
+  rm out${test}.dat
 done
 
 for test in "${!mpi_ranks[@]}" ; do
