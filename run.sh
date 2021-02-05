c22c58fb89c749818462ebd88f2f7a9a1dc473d7
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-02-05 13:12:52 +0100

    Avoid creating coreneuron data in all tests and fix patstim online mode (with proper fix in NEURON)

--- run.sh
+++ run.sh
@@ -14,20 +14,12 @@ mpi_ranks["patstim"]=2
 mpi_ranks["vecplay"]=2
 mpi_ranks["watch"]=2
 
-for test in "${!mpi_ranks[@]}" ; do
-  mkdir -p test${test}dat
-done
-
 ~/bbp_repos/nrn/build/install/bin/nrnivmodl -coreneuron mod
 
 for test in "${!mpi_ranks[@]}" ; do
   echo "Running neuron for $test"
   num_ranks=${mpi_ranks[$test]}
-  if [ "${test}" = "patstim" ]; then
-    mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 -c dumpmodel=1 test${test}.hoc
-  else
-    mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 test${test}.hoc
-  fi
+  mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 test${test}.hoc
   cat out${test}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${test}.spk
   rm out${test}.dat
 done
@@ -35,13 +27,7 @@ done
 #prepare coreneuron executable
 for test in "${!mpi_ranks[@]}" ; do
   echo "Running coreneuron for $test"
-  num_ranks=${mpi_ranks[$test]}
-  if [ "${test}" = "patstim" ]; then
-    mpirun -n $num_ranks ./x86_64/special-core --mpi --mpi -d coredat -e 100 --pattern patstim.spk
-    mv out.dat out${test}.dat
-  else
-    mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 -c coreneuron=1 test${test}.hoc
-  fi
+  mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 -c coreneuron=1 test${test}.hoc
   cat out${test}.dat | sort -k 1n,1n -k 2n,2n > out_cn_${test}.spk
   rm out${test}.dat
 done
