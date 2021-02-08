5fd83f6dcee42e4eb9386a38b580b15d738e2770
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-02-08 14:31:38 +0100

    Added spike comparison with the reference spikes to the run.sh

--- run.sh
+++ run.sh
@@ -24,6 +24,7 @@ mpi_ranks["netstimdirect"]=2
 EXTRA_ARGS=""
 GPU_ARG="-c gpu=1"
 
+# Run tests that require separate runs of NEURON and CoreNEURON to compare their spikes only
 for test in $spike_comparison_tests; do
   echo "Running neuron for $test"
   num_ranks=${mpi_ranks[$test]}
@@ -48,17 +49,36 @@ for test in $spike_comparison_tests; do
   rm out${test}.dat
 done
 
+# Compare the spikes of the spike_comparison_test with the reference spikes
 for test in $spike_comparison_tests; do
-  DIFF=$(diff -w -q out_nrn_${test}.spk out_cn_${test}.spk)
-  if [ "$DIFF" != "" ]
+  DIFF_NRN=$(diff -w -q out_nrn_${test}.spk reference/out_${test}.spk)
+  if [ "$DIFF_NRN" != "" ]
   then
-    echo "Test ${test} failed"
+    echo "Test ${test} with NEURON failed"
+    exit 1
+  fi
+  DIFF_CORENRN=$(diff -w -q out_cn_${test}.spk reference/out_${test}.spk)
+  if [ "$DIFF_CORENRN" != "" ]
+  then
+    echo "Test ${test} with CoreNEURON failed"
+    exit 1
   fi
 done
 
+# Run the direct_tests that compare internally NEURON and CoreNEURON simulations
 for test in $direct_tests; do
   echo "Running neuron and coreneuron for $test"
   num_ranks=${mpi_ranks[$test]}
   mpirun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 test${test}.hoc
   cat out${test}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${test}.spk
+done
+
+# Compare the spikes of the direct_tets with the reference spikes
+for test in $direct_tests; do
+  DIFF_NRN=$(diff -w -q out_nrn_${test}.spk reference/out_${test}.spk)
+  if [ "$DIFF_NRN" != "" ]
+  then
+    echo "Test ${test} with NEURON/CoreNEURON failed"
+    exit 1
+  fi
 done
